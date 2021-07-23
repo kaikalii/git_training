0b2ae524b3afc163986c27ab765cfddff0920e86
Kai Schmidt <kaikaliischmidt@gmail.com>
2021-07-23 11:07:21 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace git_training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
