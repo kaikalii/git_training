a71f2dbc080a253529b2f3ab9d8d6419eed87a7d
Kai Schmidt <kaikaliischmidt@gmail.com>
2021-07-23 11:16:37 -0700

    Added "kai was here"

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace git_training
         {
             Console.WriteLine("Hello everyone!");
             Console.WriteLine("Hello everyone!");
+
+            Console.WriteLine("Kai was here");
         }
     }
 }
